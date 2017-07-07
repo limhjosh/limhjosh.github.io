70b2d2d803a800eee0e6e60162d9175b7fbec0c0
Josh Lim <joshielim@gmail.com>
2017-07-06 21:36:46 -0700

    Add jQuery description of the icons, with a reset feature

--- js/application.js
+++ js/application.js
@@ -1,18 +1,22 @@
 $( document ).ready(function() {
 
 	$("body").on("hover", ".github", function(){
+		$(".description").show();
 	    $(".description").text("Github");
 	});
 
 	$("body").on("hover", ".linkedin", function(){
+		$(".description").show();
 	    $(".description").text("Linkedin");
 	});
 
 	$("body").on("hover", ".resume", function(){
+		$(".description").show();
 	    $(".description").text("Resume");
 	});
 
 	$("body").on("hover", ".email", function(){
+		$(".description").show();
 	    $(".description").text("Email");
 	});
 	
