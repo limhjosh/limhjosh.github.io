4cec6d0f3ca36f394654d0ce4fc2e8292de20ffd
Josh Lim <joshielim@gmail.com>
2017-07-19 17:45:28 -0700

    Implement animated scrolling

--- js/application.js
+++ js/application.js
@@ -47,8 +47,7 @@ $( document ).ready(function() {
 	
 	$("body").on("click", ".bounce", function() {
     		$('html,body').animate({
-//         		scrollTop: $(".end-image").offset().top}, 14000);
-			scrollTop(1000).offset().top}, 4000);
+        		scrollTop: $(".bio").offset().top}, 4000);
 		$(".bounce").hide()
 	});
 });
